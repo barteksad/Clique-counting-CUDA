fd3a9522ba1d6a949fe2b10343ce74db845abadb
barteksad <barteksadlej@gmail.com>
2023-04-05 08:40:51 +0200

    read input

---
+++ src/kcliques.cu
@@ -0,0 +1,12 @@
+#include <./common/input.h>
+
+// #include <cooperative_groups/scan.h>
+
+int main(int argc, char *argv[]) {
+  const int K = std::stoi(argv[2]);
+
+  const input_t input = read_input(argv[1]);
+
+  // compute histogram to count nodes degrees
+  
+}
