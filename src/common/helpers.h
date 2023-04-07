f52b81c1a3229d3d91cca6d06a5ba9b2ff614f03
barteksad <barteksadlej@gmail.com>
2023-04-07 21:26:25 +0200

    part of dfs

--- src/common/helpers.h
+++ src/common/helpers.h
@@ -22,4 +22,26 @@ static void HandleError( cudaError_t err,
                                     __FILE__, __LINE__ ); \
                             exit( EXIT_FAILURE );}}
 
+__device__ uint32_t bsearch_dev(const uint32_t *array, const uint32_t key, const uint32_t size) {
+    uint32_t low = 0;
+    uint32_t high = size;
+    uint32_t mid;
+    while (low < high) {
+        mid = (low + high) / 2;
+        // printf("low: %d, high: %d, mid: %d\n", low, high, mid);
+        if (array[mid] < key) {
+            low = mid + 1;
+        } else if (array[mid] > key) {
+            high = mid;
+        } else {
+            return mid;
+        }
+    }
+    if(array[low] == key) {
+        return low;
+    } else {
+        return UINT32_MAX;
+    }
+}
+
 #endif  // __HELPERS_H__
--- src/kcliques.cu
+++ src/kcliques.cu
@@ -1,14 +1,14 @@
+#include <stdlib.h>
+#include <thrust/sort.h>
+
 #include "./common/input.h"
 #include "./common/helpers.h"
 #include "./common/timers.h"
-
-#include <thrust/sort.h>
+#include "./common/cpu_tests.h"
 
 #ifdef DEBUG
 #include <cassert>
-#include "./common/cpu_tests.h"
 #endif // DEBUG
-// #include <cooperative_groups/scan.h>
 
 #define HIST_ELEM_PER_THREAD 100
 #define HIST_THREADS 1024
@@ -46,6 +46,147 @@ struct CSR {
   uint32_t n_edges;
 };
 
+#define D_OUT_MAX 1024
+#define SUB_WARP_SIZE 32
+// OA - orientation approach
+#define OA_WARP_GROUPS 4
+#define OA_THREADS OA_WARP_GROUPS * SUB_WARP_SIZE
+static_assert(SUB_WARP_SIZE && !(SUB_WARP_SIZE & (SUB_WARP_SIZE - 1)));
+static_assert(D_OUT_MAX % 32 == 0);
+static_assert(D_OUT_MAX % (SUB_WARP_SIZE * OA_WARP_GROUPS) == 0);
+#define K_MAX 11
+__global__ void graph_orientation_approach(
+  CSR csr, uint32_t K) {
+  __shared__ uint32_t K_local[K_MAX - 2]; 
+  __shared__ uint32_t induced_sub_graph[D_OUT_MAX / 32][D_OUT_MAX / 32];
+  // create mapping between original vertex index and index in induced subgraph
+  // eg. vertexes [11, 100, 123] -> at indexes [0, 1, 2]
+  // then we can binsearch to find index of neighbor in induced subgraph
+  __shared__ uint32_t vertex2idx[D_OUT_MAX];
+
+  uint32_t vertex_idx = blockIdx.x;
+  uint32_t tid = threadIdx.x;
+  uint32_t sub_warp_id = tid / SUB_WARP_SIZE;
+  uint32_t sub_warp_tid = tid % SUB_WARP_SIZE;
+
+  // initialize K_local
+  for(int i = tid; i < K_MAX - 2; i += blockDim.x) {
+    K_local[i] = 0;
+  }
+  
+  for(int row = sub_warp_id; row < D_OUT_MAX / 32; row += OA_WARP_GROUPS) {
+    for(int col = sub_warp_tid; col < D_OUT_MAX / 32; col += SUB_WARP_SIZE) {
+      induced_sub_graph[row][col] = 0;
+    }
+  }
+
+  for(int i = tid; i < D_OUT_MAX; i += blockDim.x) {
+    vertex2idx[i] = UINT32_MAX;
+  }
+  __syncthreads();
+
+  for(uint32_t i = csr.row_array[vertex_idx] + tid; i < csr.row_array[vertex_idx + 1]; i += blockDim.x) {
+    vertex2idx[i - csr.row_array[vertex_idx]] = csr.col_array[i];
+  }
+  __syncthreads();
+
+  // create induced subgraph indicating connection between nodes and store it in shared memory as bitmap, map vertex indexes to interval [0, vertex degree]
+  // warp per neighbor
+  uint32_t n_neighbors = csr.row_array[vertex_idx + 1] - csr.row_array[vertex_idx];
+  for(uint32_t neighbor_i = sub_warp_id; neighbor_i < n_neighbors; neighbor_i += OA_WARP_GROUPS) {
+    uint32_t neighbor = csr.col_array[csr.row_array[vertex_idx] + neighbor_i];
+    for(uint32_t i = csr.row_array[neighbor] + sub_warp_tid; i < csr.row_array[neighbor + 1]; i += SUB_WARP_SIZE) {
+      uint32_t neighbor_neighbor_vertex = csr.col_array[i];
+      uint32_t neighbor_neighbor_idx = bsearch_dev(vertex2idx, neighbor_neighbor_vertex, n_neighbors);
+      if(neighbor_neighbor_idx != UINT32_MAX) {
+        atomicOr(&induced_sub_graph[neighbor_i][neighbor_neighbor_idx / 32], 1 << (neighbor_neighbor_idx % 32));
+      }
+    }
+  }
+  __syncthreads();
+
+  __shared__ uint32_t next_possible_neighbor[OA_WARP_GROUPS][K_MAX - 3];
+  __shared__ uint32_t dfs_vertex[OA_WARP_GROUPS][K_MAX - 3];
+  __shared__ uint32_t intersections[OA_WARP_GROUPS][K_MAX - 3][D_OUT_MAX / 32];
+  for(uint32_t warp_subtree = sub_warp_id; warp_subtree < n_neighbors; warp_subtree += OA_WARP_GROUPS) {
+    for(uint32_t i = sub_warp_tid; i < K_MAX - 3; i += SUB_WARP_SIZE) {
+      next_possible_neighbor[sub_warp_id][i] = 0;
+    }
+    if(sub_warp_tid == 0) {
+      dfs_vertex[sub_warp_id][0] = warp_subtree;
+    }
+    __syncwarp();
+
+    uint32_t curr_k = 3;
+    while(true) {
+      // find first non zero bit in to_check >= prev_check
+      uint32_t prev = next_possible_neighbor[sub_warp_id][curr_k - 3];
+      __syncwarp();
+      if(sub_warp_tid) {
+        next_possible_neighbor[sub_warp_id][curr_k - 3] = UINT32_MAX;
+      }
+      __syncwarp();
+
+      for(uint32_t i = sub_warp_tid; i < D_OUT_MAX / 32; i += SUB_WARP_SIZE) {
+        uint32_t x = induced_sub_graph[dfs_vertex[sub_warp_id][curr_k - 3]][i];
+        if(x == 0) {
+          continue;
+        } else {
+          int pos = __ffs(x) - 1;
+          if(pos == -1 || i * 32 + pos <= prev) {
+            continue;
+          } else {
+            atomicMin(&next_possible_neighbor[sub_warp_id][curr_k - 3], i * 32 + pos);
+          }
+        }
+      }
+      __syncwarp();
+
+      // if no next possible neighbor, backtrack
+      if(next_possible_neighbor[sub_warp_id][curr_k - 3] == UINT32_MAX) {
+        // backtrack
+        curr_k--;
+        if(curr_k == 2) {
+          break;
+        }
+        continue;
+      }
+      
+      // increment kclique counter
+      atomicAdd(&K_local[curr_k - 3], 1);
+
+    }
+  }
+  // __shared__ uint32_t to_check[OA_WARP_GROUPS][D_OUT_MAX / 32];
+  // for(uint32_t warp_subtree = sub_warp_id; warp_subtree < n_neighbors; warp_subtree += OA_WARP_GROUPS) {
+  //   // zero to_check
+  //   for(uint32_t i = sub_warp_tid; i < D_OUT_MAX / 32; i += SUB_WARP_SIZE) {
+  //     to_check[sub_warp_id][i] = 0;
+  //   }
+  //   __syncwarp();
+  //   // set to check to next subtree
+  //   if(sub_warp_tid = 0) {
+  //     to_check[sub_warp_id][warp_subtree / 32] = 1 << (warp_subtree % 32);
+  //   }
+  //   __syncwarp();
+
+  //   for(uint32_t curr_k = 3; curr_k <= K; curr_k++) {
+
+  //     int 
+  //     // count number of cliques of size curr_k
+  //     for(uint32_t i = sub_warp_tid; i < n_neighbors; i += SUB_WARP_SIZE) {
+  //       uint32_t n_cliques = __popc(induced_sub_graph[warp_subtree][warp_subtree / 32]);
+  //     }
+  //   }
+  // }
+
+  // print induced subgraph
+  if(tid == 0) {
+    printf("vertex %u: , induced[0][0]: %d induced[1][0]: %d, induced[2][0]: %d\n", vertex_idx, induced_sub_graph[0][0], induced_sub_graph[1][0], induced_sub_graph[2][0]);
+  }
+}
+
+
 size_t N_STREAMS = 2;
 
 void sync_streams(cudaStream_t *streams, int n_streams) {
@@ -58,6 +199,7 @@ int main(int argc, char *argv[]) {
   CpuTimer timer("main");
 
   const int K = std::stoi(argv[2]);
+  const std::string out_file_path = argv[3];
 
   const auto [A, B, n_vertex] = read_input(argv[1]);
   const int n_edges = A.size();
@@ -112,8 +254,8 @@ int main(int argc, char *argv[]) {
   std::cout << "Histograms on CPU and GPU match!\n";
   cudaFreeHost(host_hist);
   #endif // NDEBUG
-
   // -- end of histogram computation
+
 
   // graph orientation
   {
@@ -123,6 +265,7 @@ int main(int argc, char *argv[]) {
     graph_orientation<<<orient_blocks, ORIENT_THREADS, 0, streams[0]>>>(dev_a, dev_b, n_edges, dev_hist);
     sync_streams(streams, 1);
   }
+  // -- end of graph orientation
 
   // create CSR format
   // edges_b will become col_array and edges_a will be used to compute row_array which we can store in dev_hist memory since it is not needed anymore
@@ -156,7 +299,20 @@ int main(int argc, char *argv[]) {
   HANDLE_ERROR(cudaMemcpyAsync(host_col_array, csr.col_array, n_edges * sizeof(uint32_t), cudaMemcpyDeviceToHost, streams[1]));
   sync_streams(streams, 2);
 
-  for(auto i = 0; i < n_vertex; i++) {
+  std::cout << "CSR row array:\n";
+  for(auto i = 0; i < CSR_row_size; i++) {
+    std::cout << host_row_array[i] << " ";
+  }
+  std::cout << "\n";
+  std::cout << "CSR col array:\n";
+  for(auto i = 0; i < n_edges; i++) {
+    std::cout << host_col_array[i] << " ";
+  }
+  std::cout << "\n";
+  std::cout << "CSR n_vertex: " << n_vertex << "\n";
+  std::cout << "CSR n_edges: " << n_edges << "\n";
+
+  for(auto i = 0; i < CSR_row_size; i++) {
     assert(row_array[i] == host_row_array[i]);
   }
   for(auto i = 0; i < n_edges; i++) {
@@ -168,6 +324,12 @@ int main(int argc, char *argv[]) {
   cudaFreeHost(host_col_array);
   #endif // DEBUG
 
+  // orientation approach
+  {
+    CudaTimer oa_timer("orientation_approach");
+    graph_orientation_approach<<<n_vertex, OA_THREADS, 0, streams[0]>>>(csr, K);
+    sync_streams(streams, 1);
+  }
 
   cudaFree(csr.col_array); // dev_b
   cudaFree(csr.row_array); // dev_hist
