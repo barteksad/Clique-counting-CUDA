83c03e7e87eed3b0b411fdd95def91fca78a1e1c
barteksad <barteksadlej@gmail.com>
2023-04-22 16:19:51 +0200

    comments

--- src/kcliques.cu
+++ src/kcliques.cu
@@ -47,45 +47,52 @@ struct CSR {
   uint32_t n_edges;
 };
 
-#define D_OUT_MAX 1024U
-#define SUB_WARP_SIZE 8U
+#define D_OUT_MAX 1024U // max degree of output graph
+#define SUB_WARP_SIZE 8U // number of threads per sub warp, what is described in paper as threads group
 // OA - orientation approach
-#define OA_WARP_GROUPS 128U
-#define OA_THREADS OA_WARP_GROUPS * SUB_WARP_SIZE
-static_assert(SUB_WARP_SIZE && !(SUB_WARP_SIZE & (SUB_WARP_SIZE - 1)));
-static_assert(D_OUT_MAX % 32 == 0);
+#define OA_WARP_GROUPS 128U // number of thread groups per block
+#define OA_THREADS OA_WARP_GROUPS * SUB_WARP_SIZE // number of threads per block
+static_assert(SUB_WARP_SIZE && !(SUB_WARP_SIZE & (SUB_WARP_SIZE - 1))); // check if SUB_WARP_SIZE is power of 2
+static_assert(D_OUT_MAX % 32 == 0); 
 #define K_MAX 11
 
+// helper macros for indexing flat nd arrays
+#define idx_intersections(its, its_offset, row, col, k) its[its_offset + row * (K_MAX - 2) * (D_OUT_MAX / 32) + col * (K_MAX - 2) + k]
+#define idx_induced_sub_graph(isg, isg_offset, row, col) isg[isg_offset + row * (D_OUT_MAX / 32) + col]
+
 __global__ void graph_orientation_approach(
-  const CSR csr, 
-  const uint32_t vertex_idx_offset, 
+  const CSR csr, // CSR representation of graph
+  const uint32_t vertex_idx_offset, // number of kernel group currently processed
   const uint32_t K, 
   unsigned long long int K_global[K_MAX - 2], 
   uint32_t induced_sub_graph[],
-  uint32_t vertex2idx[], // [D_OUT_MAX]
-  uint32_t intersections[] // [OA_WARP_GROUPS][K_MAX - 2][D_OUT_MAX / 32]
+  uint32_t vertex2idx[], // shape: [D_OUT_MAX]
+  // for storing intersections per group at each recurrence level, shape: [OA_WARP_GROUPS][K_MAX - 2][D_OUT_MAX / 32]
+  uint32_t intersections[] 
   ) {
   __shared__ unsigned long long int K_local[K_MAX - 2]; 
 
+  // split warps into sub warps
   cg::thread_group tile = cg::tiled_partition(cg::this_thread_block(), SUB_WARP_SIZE);
 
-  const uint32_t vertex_idx = vertex_idx_offset + blockIdx.x;
+  const uint32_t vertex_idx = vertex_idx_offset + blockIdx.x; // index of vertex in original graph
   const uint32_t tid = threadIdx.x;
-  const uint32_t sub_warp_id = tid / SUB_WARP_SIZE;
-  const uint32_t sub_warp_tid = tile.thread_rank();
-  const size_t isg_offset = blockIdx.x * D_OUT_MAX * (D_OUT_MAX / 32);
-  const size_t v2i_offset = blockIdx.x * D_OUT_MAX;
-  const size_t its_offset = blockIdx.x * OA_WARP_GROUPS * (K_MAX - 2) * (D_OUT_MAX / 32);
+  const uint32_t sub_warp_id = tid / SUB_WARP_SIZE; // id in threads group
+  const uint32_t sub_warp_tid = tile.thread_rank(); // id of threads group
+  const size_t isg_offset = blockIdx.x * D_OUT_MAX * (D_OUT_MAX / 32); // offset in induced_sub_graph array
+  const size_t v2i_offset = blockIdx.x * D_OUT_MAX; // offset in vertex2idx array
+  const size_t its_offset = blockIdx.x * OA_WARP_GROUPS * (K_MAX - 2) * (D_OUT_MAX / 32); // offset in intersections array
   assert(vertex_idx < csr.n_vertex);
   assert(K <= K_MAX);
   // initialize K_local
   for(int i = tid; i < K_MAX - 2; i += blockDim.x) {
     K_local[i] = 0;
   }
-  
+
+  // initialize induced_sub_graph
   for(int row = sub_warp_id; row < D_OUT_MAX; row += OA_WARP_GROUPS) {
     for(int col = sub_warp_tid; col < D_OUT_MAX / 32; col += SUB_WARP_SIZE) {
-      induced_sub_graph[isg_offset + row * (D_OUT_MAX / 32) + col] = 0;
+      idx_induced_sub_graph(induced_sub_graph, isg_offset, row, col) = 0;
     }
   }
 
@@ -93,7 +100,7 @@ __global__ void graph_orientation_approach(
   // eg. vertexes [11, 100, 123] -> at indexes [0, 1, 2]
   // then we can binsearch to find index of neighbor in induced subgraph
   for(int i = tid; i < D_OUT_MAX; i += blockDim.x) {
-    vertex2idx[v2i_offset + i] = UINT32_MAX;
+    vertex2idx[v2i_offset + i] = UINT32_MAX; // initialize with out of range value
   }
   __syncthreads();
 
@@ -108,38 +115,45 @@ __global__ void graph_orientation_approach(
   assert(n_neighbors <= D_OUT_MAX);
   if(n_neighbors == 0)
     return;
+  // for each neighbor do
   for(uint32_t neighbor_i = sub_warp_id; neighbor_i < n_neighbors; neighbor_i += OA_WARP_GROUPS) {
     assert(csr.row_array[vertex_idx] + neighbor_i < csr.row_array[vertex_idx + 1]);
     const uint32_t neighbor = csr.col_array[csr.row_array[vertex_idx] + neighbor_i];
+    // for each of neighbor neighbors do
     for(uint32_t i = csr.row_array[neighbor] + sub_warp_tid; i < csr.row_array[neighbor + 1]; i += SUB_WARP_SIZE) {
       const uint32_t neighbor_neighbor_vertex = csr.col_array[i];
-      const uint32_t neighbor_neighbor_idx = bsearch_dev(vertex2idx + v2i_offset, neighbor_neighbor_vertex, n_neighbors);
+      // find index of neighbor neighbor in vertex2idx array, it becomes its index in induced subgraph
+      const uint32_t neighbor_neighbor_idx = bsearch_dev(vertex2idx + v2i_offset, neighbor_neighbor_vertex, n_neighbors); 
       assert(neighbor_neighbor_idx < n_neighbors || neighbor_neighbor_idx == UINT32_MAX);
+      // if neighbor neighbor is neighbor of vertex, set bit in induced subgraph
       if(neighbor_neighbor_idx != UINT32_MAX) {
         assert(neighbor_neighbor_idx / 32 < D_OUT_MAX / 32);
-        atomicOr(&induced_sub_graph[isg_offset + neighbor_i * (D_OUT_MAX / 32) + neighbor_neighbor_idx / 32], 1 << (neighbor_neighbor_idx % 32));
+        atomicOr(&idx_induced_sub_graph(induced_sub_graph, isg_offset, neighbor_i, neighbor_neighbor_idx / 32), 1 << (neighbor_neighbor_idx % 32));
       }
     }
   }
   __syncthreads();
 
+  // for each group and recurrence level, store the number of lowest not checked neighbor
   __shared__ uint32_t curr_neighbor_to_check[OA_WARP_GROUPS][K_MAX - 2];
+  // used to find next possible neighbor at each level
   __shared__ uint32_t next_possible_neighbor[OA_WARP_GROUPS];
-  // __shared__ uint32_t intersections[OA_WARP_GROUPS][K_MAX - 2][D_OUT_MAX / 32];
 
   for(uint32_t warp_subtree = sub_warp_id; warp_subtree < n_neighbors; warp_subtree += OA_WARP_GROUPS) {
+    // initialize curr_neighbor_to_check with 0 since we haven't checked any neighbor yet
     for(uint32_t i = sub_warp_tid; i < K_MAX - 2; i += SUB_WARP_SIZE) {
       curr_neighbor_to_check[sub_warp_id][i] = 0;
     }
 
+    // copy fresh induced subgraph to group memory
     for(uint32_t i = sub_warp_tid; i <= (n_neighbors - 1) / 32; i += SUB_WARP_SIZE) {
-      // intersections[sub_warp_id][0][i] = induced_sub_graph[isg_offset + warp_subtree * (D_OUT_MAX / 32) + i];
-      intersections[its_offset + sub_warp_id * (K_MAX - 2) * (D_OUT_MAX / 32) + i] = induced_sub_graph[isg_offset + warp_subtree * (D_OUT_MAX / 32) + i];
+      idx_intersections(intersections, its_offset, sub_warp_id, 0, i) = idx_induced_sub_graph(induced_sub_graph, isg_offset, warp_subtree, i);
     }
 
     tile.sync();
+    // count 3-cliques
     for(uint32_t i = sub_warp_tid; i <= (n_neighbors - 1) / 32; i += SUB_WARP_SIZE) {
-      uint64_t n_neighbors_k = __popc(intersections[its_offset + sub_warp_id * (K_MAX - 2) * (D_OUT_MAX / 32) + i]);
+      uint64_t n_neighbors_k = __popc(idx_intersections(intersections, its_offset , sub_warp_id, 0, i));
       if(n_neighbors_k > 0) {
         atomicAdd(&K_local[0], n_neighbors_k);
       }
@@ -159,16 +173,18 @@ __global__ void graph_orientation_approach(
       // find first non zero bit in to_check >= prev_check
       uint32_t prev = curr_neighbor_to_check[sub_warp_id][curr_k - 3];
       if(sub_warp_tid == 0) {
-        next_possible_neighbor[sub_warp_id] = UINT32_MAX;
+        next_possible_neighbor[sub_warp_id] = UINT32_MAX; // initialize with out of range value
       }
       tile.sync();
 
       for(uint32_t i = sub_warp_tid; i <= (n_neighbors - 1) / 32; i += SUB_WARP_SIZE) {
         // uint32_t x = intersections[sub_warp_id][curr_k - 3][i];
-        uint32_t x = intersections[its_offset + sub_warp_id * (K_MAX - 2) * (D_OUT_MAX / 32) + (curr_k - 3) * (D_OUT_MAX / 32) + i];
+        uint32_t x = idx_intersections(intersections, its_offset, sub_warp_id, (curr_k - 3), i);
         if(x == 0) {
           continue;
         } else {
+          // find first bit set >= prev
+          // mask is used to set all bits < prev to 0 in array index i
           uint32_t mask = UINT32_MAX;
           if(prev / 32 == i) {
             mask ^= (~(~0 << (prev % 32)));
@@ -199,17 +215,20 @@ __global__ void graph_orientation_approach(
 
       tile.sync();
 
+      // in next recursion level, start checking from next + 1
       if(sub_warp_tid == 0) {
         curr_neighbor_to_check[sub_warp_id][curr_k - 3] = next + 1;
       }
 
+      // compute intersection of induced subgraph of next neighbor and intersection of previous level
       for(uint32_t i = sub_warp_tid; i <= (n_neighbors - 1) / 32; i += SUB_WARP_SIZE) {
-        intersections[its_offset + sub_warp_id * (K_MAX - 2) * (D_OUT_MAX / 32) + (curr_k - 2) * (D_OUT_MAX / 32) + i] = intersections[its_offset + sub_warp_id * (K_MAX - 2) * (D_OUT_MAX / 32) + (curr_k - 3) * (D_OUT_MAX / 32) + i] & induced_sub_graph[isg_offset + next * (D_OUT_MAX / 32) + i];
+        idx_intersections(intersections, its_offset, sub_warp_id, (curr_k - 2), i) = idx_intersections(intersections, its_offset, sub_warp_id, (curr_k - 3), i) & idx_induced_sub_graph(induced_sub_graph, isg_offset, next, i);
       }
       tile.sync();
 
+      // count k-cliques
       for(uint32_t i = sub_warp_tid; i <= (n_neighbors - 1) / 32; i += SUB_WARP_SIZE) {
-        uint64_t n_neighbors_k = __popc(intersections[its_offset + sub_warp_id * (K_MAX - 2) * (D_OUT_MAX / 32) + (curr_k - 2) * (D_OUT_MAX / 32) + i]);
+        uint64_t n_neighbors_k = __popc(idx_intersections(intersections, its_offset, sub_warp_id, (curr_k - 2), i));
         if(n_neighbors_k > 0) {
           atomicAdd(&K_local[curr_k - 2], n_neighbors_k);
         }
